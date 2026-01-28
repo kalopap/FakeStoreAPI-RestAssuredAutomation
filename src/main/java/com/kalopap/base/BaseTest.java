e450bb19276d685fad8db93cab6452cfebc1bf84
kalopap <reachkalyanihere@hotmail.com>
2026-01-27 20:20:45 -0600

    Added Allure Reports to the test results

--- src/main/java/com/kalopap/base/BaseTest.java
+++ src/main/java/com/kalopap/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.kalopap.base;
 
 import com.kalopap.utils.ConfigReader;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
@@ -13,6 +14,7 @@ public class BaseTest {
     public void setup(@Optional("DEV") String env){
         ConfigReader.setEnv(env);
         RestAssured.baseURI = ConfigReader.getBaseUrl();
+        RestAssured.filters(new AllureRestAssured());
     }
 
 }
